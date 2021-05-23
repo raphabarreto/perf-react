e08a0bce4b9550c2f340575e12074ded9b45a49f
Raphael Barreto <tnt.raphael@gmail.com>
2021-05-22 21:05:18 -0300

    Virtualização

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,4 +1,5 @@
-import { useMemo } from 'react';
+import { List, ListRowRenderer } from 'react-virtualized';
+
 import { ProductItem } from '../components/ProductItem';
 
 interface SearchResultsProps {
@@ -17,19 +18,29 @@ export function SearchResults({
   results,
   onAddToWhishlist,
 }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWhishlist={onAddToWhishlist}
+        />
+      </div>
+    );
+  };
+
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      {results.map((product) => {
-        return (
-          <ProductItem
-            key={product.id}
-            product={product}
-            onAddToWhishlist={onAddToWhishlist}
-          />
-        );
-      })}
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   );
 }
